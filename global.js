4a51ea66542b69a962f4eca14780bb8b5fe0dce6
Ani <64718589+AniBoppana@users.noreply.github.com>
2026-04-17 11:45:34 -0700

    replaced nav logic in js

--- global.js
+++ global.js
@@ -4,10 +4,39 @@ function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
 
-const navLinks = $$("nav a");
+const pages = [
+  { url: '', title: 'Home' },
+  { url: 'projects/', title: 'Projects' },
+  { url: 'contact/', title: 'Contact' },
+  { url: 'resume/', title: 'Resume' },
+  { url: 'https://github.com/AniBoppana', title: 'GitHub' }
+];
 
-const currentLink = navLinks.find(
-  (a) => a.host === location.host && a.pathname === location.pathname
-);
 
-currentLink?.classList.add('current');
+const BASE_PATH =
+  (location.hostname === "localhost" || location.hostname === "127.0.0.1")
+    ? "/"
+    : "/portfolio/";
+
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+
+for (let p of pages) {
+  let url = p.url;
+  if (!url.startsWith('http')) {
+    url = BASE_PATH + url;
+  }
+  let a = document.createElement('a');
+  a.href = url;
+  a.textContent = p.title;
+
+  a.classList.toggle(
+    'current',
+    a.host === location.host && a.pathname === location.pathname
+  );
+
+  a.toggleAttribute('target', a.host !== location.host);
+  if (a.target) a.rel = "noopener";
+
+  nav.append(a);
+}
