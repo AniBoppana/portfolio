040c0da00a6ae114fa382a8a13bec57e5ebe9d0c
Ani <64718589+AniBoppana@users.noreply.github.com>
2026-05-08 11:35:51 -0700

    edited axis

--- meta/main.js
+++ meta/main.js
@@ -112,12 +112,25 @@ function renderScatterPlot(data, commits) {
     .attr('r', 5)
     .attr('fill', d => d3.interpolateCool(d.hourFrac / 24));
 
-  // X Axis
   svg.append('g')
+    .attr('class', 'x-axis-months')
     .attr('transform', `translate(0, ${usableArea.bottom})`)
-    .call(d3.axisBottom(xScale));
+    .call(
+      d3.axisBottom(xScale)
+        .ticks(d3.timeMonth.every(1))
+        .tickFormat(d3.timeFormat('%b'))
+    );
 
-  // Y Axis
+  svg.append('g')
+    .attr('class', 'x-axis-days')
+    .attr('transform', `translate(0, ${usableArea.bottom + 20})`)
+    .call(
+      d3.axisBottom(xScale)
+        .ticks(d3.timeDay.every(1))
+        .tickFormat(d => d.getDate() === 1 ? '' : d3.timeFormat('%d %a')(d))
+        .tickSize(4)
+    );
+
   svg.append('g')
     .attr('transform', `translate(${usableArea.left}, 0)`)
     .call(
